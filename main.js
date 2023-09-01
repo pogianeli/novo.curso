96eb17bd924bdd9997805c7a2d5c15d47397269b
pogianeli <130576465+pogianeli@users.noreply.github.com>
2023-09-01 11:35:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ for (let indice = 0; indice < listaDeTeclas.length; indice++) {
     inputTel.value = inputTel.value + tecla.value;
   }
 }
+const listaDeTeclas = document.querySelectorAll('input[type=button]');
+
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+  const tecla = listaDeTeclas[indice];
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space") {
+    tecla.classList.add('ativa');
+    }
+  }
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+}
